e90f98abcf51834694e2657492f7c24d7d497ea9
Casey Densmore <cdensmore101@gmail.com>
2020-05-13 12:41:15 -0400

    Splash screen implementation

--- main.py
+++ main.py
@@ -5,14 +5,27 @@
 #     Purpose: Main (launcher) script for AXBT Realtime Editing System (ARES). 
 # =============================================================================
 
-
-#Import and run main program
+#import and run splash screen
 from sys import argv, exit
-from PyQt5.QtWidgets import QApplication
-import ARESgui 
+from PyQt5.QtWidgets import QApplication, QSplashScreen
+from PyQt5.QtGui import QPixmap
+import time
 
 app = QApplication(argv)
+splash = QSplashScreen(QPixmap("qclib/dropicon.png"))
+splash.show()
+
+#Imports necessary for main program
+import ARESgui 
+
+#creates main program instance
+app2 = QApplication(argv)
 ex = ARESgui.RunProgram()
-exit(app.exec_())
+
+#kill splash screen
+splash.close()
+
+#executes main program
+exit(app2.exec_())
     
     
