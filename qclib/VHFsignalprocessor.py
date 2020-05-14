505702a3aa028860a3040ee0400a2cd14dbc2270
Casey Densmore <cdensmore101@gmail.com>
2020-05-14 18:11:06 -0400

    Included notices for GNU General Public License v3

--- qclib/VHFsignalprocessor.py
+++ qclib/VHFsignalprocessor.py
@@ -6,6 +6,20 @@
 #       converting AXBT data into temperature/depth information, either in real-
 #       time with WiNRADIO receivers, or reprocessing raw audio data from WAV files
 #
+#
+#    ARES is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    ARES is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with ARES.  If not, see <https://www.gnu.org/licenses/>.
+#
 #       **NOTE 1**: This module handles all communication with the WiNRADIO receivers
 #       through the WiNRADIO DLL/API, directly in Python using the Ctypes module.
 #       Due to platform limitations for the WiNRADIO, several of the functions/
